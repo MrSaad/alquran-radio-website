b5e365de4160fdb7996e838735d41584ef8af898
Saad Ahmed <saadem3000@gmail.com>
2016-05-22 06:44:15 -0700

    Set nodejs to listen only on localhost

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -63,3 +63,74 @@ $(function(d, s, id) {
 }(document, 'script', 'facebook-jssdk'));
 
 
+$(function(){
+    'use strict';
+    var player = {
+        "id": "2fcfe9e",
+        "name": "website-experiment",
+        "type": "medium",
+        "stream": {
+            "station ": "s25ffa1bef",
+            "output": null,
+            "streaming_url": "https:\/\/streaming.radio.co\/s25ffa1bef"
+        },
+        "theme": {
+            "width": 400,
+            "background_colour": "#fcfcfc",
+            "text_colour": "#364349",
+            "accent_colour": "#7b919d",
+            "rounded_corners": false
+        },
+        "settings": {
+            "autoplay": false,
+            "artwork": true
+        },
+        "embed_url": "https:\/\/embed.radio.co\/player\/2fcfe9e.html",
+        "social": {
+            "twitter": false,
+            "facebook_share": false,
+            "embed": true,
+            "template": "AlQuran Radio"
+        }
+    };
+    var i = document.getElementById('fixedEmbed');
+    var style = "border:none;";
+    var width = '100%'
+    if (window.screen.width < 400) {
+        width = '100%';
+    }
+    i.src = player.embed_url;
+    i.width = width;
+    if (player.theme.rounded_corners) {
+        style += 'border-radius:8px;';
+    }
+    i.setAttribute('style', style);
+    // var s = document.getElementsByTagName('script');
+    // s = s[s.length - 1];
+    // if (s.parentNode.nodeName === 'HEAD') {
+    //     window.onload = function() {
+    //         document.body.appendChild(i);
+    //     };
+    // } else {
+    //     s.parentNode.insertBefore(i, s);
+    // }
+    window.addEventListener('message', function(e) {
+        var eventName = e.data[0];
+        var data = e.data[1];
+        if (eventName === player.id + '.setHeight') {
+            i.style.height = data + 'px';
+        }
+    }, false);
+    i.addEventListener('load', function() {
+        var targetUrl = player.embed_url.split('/').splice(0, 3).join('/');
+        setTimeout(function() {
+            i.contentWindow.postMessage(JSON.stringify(['parent', location]), targetUrl);
+        }, 1000);
+    });
+}());
+
+$( window ).load(function() { 
+	console.log( $("#fixedEmbed").contents().find(".resize-w").width('100%') )
+})
+
+
